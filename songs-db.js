7c59c5dce2e2f5360e29ddf1a1c2c1877a641031
Julian Duque <julianduquej@gmail.com>
2019-04-25 09:00:49 -0700

    Implement _insert and _list methods

--- songs-db.js
+++ songs-db.js
@@ -10,6 +10,41 @@ class SongsDb extends EventEmitter {
     this.db = new Database(path.join(__dirname, dbFile))
     this.db.on('error', err => this.emit('error', err))
   }
+
+  async _insert (query, params) {
+    return new Promise((resolve, reject) => {
+      // onComplete callback needs to be a normal function
+      function onComplete (err) {
+        if (err) return reject(err)
+
+        resolve({ id: this.lastID })
+      }
+
+      this.db.run(query, params, onComplete)
+    })
+  }
+
+  async _list (query, params) {
+    return new Promise((resolve, reject) => {
+      const results = []
+
+      // Accumulate Rows
+      const onRow = (err, row) => {
+        if (err) return reject(err)
+
+        results.push(row)
+      }
+
+      // Completion Callback
+      const onComplete = (err, times) => {
+        if (err) return reject(err)
+
+        resolve(results)
+      }
+
+      this.db.each(query, params, onRow, onComplete)
+    })
+  }
 }
 
 module.exports = SongsDb
